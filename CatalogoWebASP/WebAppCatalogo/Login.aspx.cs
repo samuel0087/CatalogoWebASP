d407364414b6bc20d00b49bac4c12538405677fc
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2026-05-03 21:23:53 -0300

    se creo los estilos para la pantalla de login

--- CatalogoWebASP/WebAppCatalogo/Login.aspx.cs
+++ CatalogoWebASP/WebAppCatalogo/Login.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace WebAppCatalogo
 {
-    public partial class About : Page
+    public partial class Formulario_web1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
